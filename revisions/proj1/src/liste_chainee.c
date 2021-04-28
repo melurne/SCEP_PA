0aed0384a7f5fd270c3cb5bf3693c4a77512789e
melurne <maxence1.neus@gmail.com>
2021-04-28 15:03:28 +0200

    part1

---
+++ revisions/proj1/src/liste_chainee.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"liste_chainee.h"
+
+void ajout_tete(Liste* L, int x) {
+	Cell* first = malloc(sizeof(Cell));
+	first->val = x;
+	first->next = *L;
+	L = &first;
+}
+
+void affiche(Liste L) {
+	if (L == NULL)
+		return;	
+	printf("%d, ", L->val);
+	affiche(L->next);
+}
--- revisions/proj1/src/main.c
+++ revisions/proj1/src/main.c
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include"liste_chainee.h"
 
 int main() {
-  
-
-
+	Liste* ptL1;
+	ajout_tete(ptL1, 5);
+	return 0;
 }
